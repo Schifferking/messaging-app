29c477636fa6d140d858885cee1b567c66b2d698
Schifferking <101956382+Schifferking@users.noreply.github.com>
2024-11-27 19:32:43 -0600

    Add: visibility icons to password inputs
    Users can't see the passwords their typing. With
    the new visibility icons, users will see their
    passwords. At the moment it doesn't toggle the
    password visibility, only the icons change
    accordingly when interacted.
    
    And lastly, these icons can also be utilized with
    keyboard.

--- frontend/src/components/PasswordInput/PasswordInput.jsx
+++ frontend/src/components/PasswordInput/PasswordInput.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import Input from "../Input/Input";
 import styles from "./PasswordInput.module.css";
+import Visibility from "../../assets/icons/visibility.svg?react";
+import VisibilityOff from "../../assets/icons/visibility-off.svg?react";
 
 function PasswordInput( props ) {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  // toggle password visibility
   const handleClick = () => {
+    if (isPasswordVisible) {
+      setIsPasswordVisible(false);
+    } else {
+      setIsPasswordVisible(true);
+    }
+
     ref.current.focus();
+  };
+
+  // make password visibility icons work with "enter" and "space" keys
+  const handleKeyDown = (e) => {
+    if (e.code === "Enter" || e.code === "Space") {
+      e.preventDefault();
+      handleClick();
+    }
   };
 
   return (
@@ -13,6 +32,22 @@ function PasswordInput( props ) {
         minLength={8}
         {...props}
       ></Input>
+
+      {isPasswordVisible ? (
+        <Visibility
+          className={styles["visibility"]}
+          onClick={handleClick}
+          onKeyDown={handleKeyDown}
+          tabIndex="0"
+        ></Visibility>
+      ) : (
+        <VisibilityOff
+          className={styles["visibility-off"]}
+          onClick={handleClick}
+          onKeyDown={handleKeyDown}
+          tabIndex="0"
+        ></VisibilityOff>
+      )}
     </>
   );
 }
