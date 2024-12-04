3415bdabfff0f138e5ad4959ef50199baee2f4b5
Schifferking <101956382+Schifferking@users.noreply.github.com>
2024-12-04 14:58:26 -0600

    Feat: handle user requests at backend
    Since this project utilizes a React app as
    frontend, Ruby app is set as API mode. Thus, it
    is not necessary to render views inside
    controllers. Just send responses to frontend as
    JSON.
    
    It also adds logic to handle account creation and
    deletion. It also manages a session creation and
    deletion (e.g. user logs in / out).

--- backend/app/controllers/users/registrations_controller.rb
+++ backend/app/controllers/users/registrations_controller.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  # POST /signup
+  def create
+    @user = User.new(registration_params)
+
+    if @user.save
+      render json: {email: @user.email, status: :ok }
+    else
+      render json: { error: @user.errors }
+    end
+  end
+
+  private
+
+  def registration_params
+    params.require(:formData).permit(:email, :password, :passwordRepeat)
+  end
 end
--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -1,4 +1,33 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  respond_to :json
+  before_action :configure_sign_in_params, only: [:create]
+
+  # POST /login
+  def create
+     @user = User.find_by(email: params[:formData][:email])
+     if @user
+      if @user.valid_password?(params[:formData][:password])
+        render json: { email: @user.email, status: :ok }
+        sign_in @user, store: false
+      else
+        render json: { error: "incorrect password" }
+      end
+     else
+        render json: { error: "incorrect email" }
+     end
+  end
+
+  # DELETE /logout
+   def destroy
+     sign_out current_user
+   end
+
+   protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+   def configure_sign_in_params
+     devise_parameter_sanitizer.permit(:create, keys: [formData: [:email, :password]])
+   end
 end
