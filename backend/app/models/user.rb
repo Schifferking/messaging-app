ae9e47853b56bd680cdcbf879a14991a5c5f9d81
Schifferking <101956382+Schifferking@users.noreply.github.com>
2024-12-04 14:49:53 -0600

    Add: user data validation in backend
    Despite having user data validation in frontend,
    the app is not secure enough. Some users will
    bypass that validation.
    
    With this user data validation, it will halt some
    malicious attempts. Like in frontend, it will ask
    the users to enter a valid email and a "valid"
    password. It also uses the Zxcvbn library to
    validate passwords.

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -6,4 +6,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  attr_accessor :passwordRepeat
+  validates :email, presence: { message: "enter an email" }
+  validates :email, uniqueness: { message: "email entered is already used, enter another one" }
+  validates :email, format: { with: /\A[\w.+-]+@\w+\.\w+\z/, message: "enter a valid email address" }
+
+  validates :password, presence: { message: "enter a password" }
+  validates :password, length: { minimum: 8 }
+  validate :password_score_cannot_be_equal_or_lower_than_two
+  validate :password_cannot_be_different_from_passwordRepeat
+
+  def password_score_cannot_be_equal_or_lower_than_two
+    if Zxcvbn.zxcvbn(password)["score"] <= 2
+      errors.add(:password, "score can't be equal or lower than two")
+    end
+  end
+
+  # using an instance variable to compare password equity
+  def password_cannot_be_different_from_passwordRepeat
+    if password != @passwordRepeat
+      errors.add(:password, "can't be different from passwordRepeat")
+    end
+  end
 end
